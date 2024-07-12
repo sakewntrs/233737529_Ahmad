9b83f89276533379b0b23dbc6c4b034938c7d4ce
sakewntrs <ismaelahmadggm8@gmail.com>
2024-07-12 21:27:50 +0800

    This program opens 5 websites, calculator and notepad.

---
+++ BSCRIPT01.bat
@@ -0,0 +1,15 @@
+BSCRIPT01
+
+@echo off
+start https://www.youtube.com
+start https://open.spotify.com
+start https://store.steampowered.com
+start https://www.starwars.com/databank/darth-vader?
+start https://www.leagueoflegends.com/en-pl/champions/aatrox/
+
+start calc
+start notepad
+
+pause
+
+shutdown -s
