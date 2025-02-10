b12dc34eab926c29bde9ae2576bee69cd53a813c
sakewntrs <ismaelahmadggm8@gmail.com>
2025-02-11 02:14:05 +0800

    final BLP

--- BusinessLandingPage/code.js
+++ BusinessLandingPage/code.js
@@ -1,1 +1,13 @@
-i 
+document.addEventListener("DOMContentLoaded", function() {
+    const ctaButton = document.querySelector(".cta");
+    const menuToggle = document.querySelector(".menu-toggle");
+    const navList = document.querySelector("#nav-list");
+
+    ctaButton.addEventListener("click", function() {
+        alert("Thanks for signing up! We'll contact you soon.");
+    });
+
+    menuToggle.addEventListener("click", function() {
+        navList.style.display = (navList.style.display === "flex") ? "none" : "flex";
+    });
+});
