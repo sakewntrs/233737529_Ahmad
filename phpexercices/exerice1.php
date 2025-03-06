1f89418bbf6482246641492d696935e3a08410d2
Jeanel Kaye Wakat <kaidelighted@gmail.com>
2025-03-06 16:18:11 +0800

    Variables and Operators

---
+++ phpexercices/exerice1.php
@@ -0,0 +1,15 @@
+<?php
+
+$a = 15;
+$b = 7;
+$sum = $a + $b;
+$difference = $a - $b;
+$product = $a * $b;
+$quotient = $a / $b;
+
+echo "The sum of $a and $b is $sum \n"; 
+echo "The difference of $a and $b is $difference \n"; 
+echo "The product of $a and $b is $product \n"; 
+echo "The quotient of $a and $b is $quotient \n"; 
+
+?>
