a3addf5060543e0d7548cf0b45a8e9f52cf8a814
Jeanel Kaye Wakat <kaidelighted@gmail.com>
2025-03-06 16:23:47 +0800

    Conditional Statements

---
+++ phpexercices/exerice2.php
@@ -0,0 +1,45 @@
+<?php
+
+$a = 15;
+$b = 7;
+
+
+if($a%2==0)
+{
+echo "$a is even \n";
+
+}
+else
+{
+echo "$a is odd \n";
+}
+
+if($b%2==0)
+{
+echo "$b is even \n";
+
+}
+else
+{
+echo "$b is odd \n";
+}
+
+if($a >0)
+{
+echo "$a is postive\n";
+
+}
+else
+{
+echo "$a its negative \n";
+}
+
+if($b >0)
+{
+echo "$b is postive\n";
+
+}
+else
+{
+echo "$b its negative \n";
+}
